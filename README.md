0b3b6da92cb6b2c675a91d64088205ae7eb88870
ananthakrishnanus <81955553+ananthakrishnanus@users.noreply.github.com>
2021-04-05 13:59:21 +0530

    area of the circle
    finds area of the circle

--- README.md
+++
@@ -1,2 +0,0 @@
-# area-of-a-circle
-developed by ananthakrishnan us, to find the area of circle
---
+++ hello.c
@@ -0,0 +1,4 @@
+radius= float(input("the radius of the circle is "))
+area= 3.14159*radius*radius
+print("the area of the circle is", area)
+  
